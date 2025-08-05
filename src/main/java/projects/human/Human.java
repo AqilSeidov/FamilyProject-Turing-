a52ff1566de2b6fdff81f0be36716014539488f2
Aqil Səidov <aqilasa638@gmail.com>
2025-08-05 19:39:26 +0400

    Human Class Refactored: cleaned structure and improved readability

--- src/main/java/projects/human/Human.java
+++ src/main/java/projects/human/Human.java
@@ -12,16 +12,17 @@ public class Human {
     private int birthDate;
     private int iq;
     private Family family;
-    private String[][] nonWorkingActivities;
+    private String[][] activities;
 
     public Human() {}
 
-    public Human(String name, String surname, int birthDate, int iq, Pet pet, Human mother, Human father, String[][] nonWorkingActivities) {
+    public Human(String name, String surname, int birthDate, int iq, Family family ,String[][] activities) {
         this.name = name;
         this.surname = surname;
         this.birthDate = birthDate;
         this.iq = iq;
-        this.nonWorkingActivities = nonWorkingActivities;
+        this.family = family;
+        this.activities = activities;
     }
 
     public Human(String name, String surname, int birthDate) {
@@ -30,60 +31,29 @@ public class Human {
         this.birthDate = birthDate;
     }
 
-    public Human(String name, String surname, int birthDate, Human mother, Human father) {
-        this.name = name;
-        this.surname = surname;
-        this.birthDate = birthDate;
-    }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name;}
+    public void setName(String name) { this.name = name;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getSurname() { return surname;}
+    public void setSurname(String surname) { this.surname = surname;}
 
-    public String getSurname() {
-        return surname;
-    }
+    public int getBirthDate() { return birthDate;}
+    public void setBirthDate(int birthDate) { this.birthDate = birthDate;}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(int birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public int getIq() {
-        return iq;
-    }
-
-    public void setIq(int iq) {
-        this.iq = iq;
-    }
-
+    public int getIq() { return iq;}
+    public void setIq(int iq) { this.iq = iq;}
 
     public String[][] getNonWorkingActivities() {
-        return nonWorkingActivities;
+        return activities != null ? Arrays.copyOf(activities, activities.length) : null;
     }
 
     public void setNonWorkingActivities(String[][] nonWorkingActivities) {
-        this.nonWorkingActivities = nonWorkingActivities;
+        this.activities = activities != null ? Arrays.copyOf(activities, activities.length) : null;
     }
 
-    public Family getFamily() {
-        return family;
-    }
-
-    public void setFamily(Family family) {
-        this.family = family;
-    }
+    public Family getFamily() { return family;}
+    public void setFamily(Family family) { this.family = family;}
 
     @Override
     public boolean equals(Object o) {
@@ -104,7 +74,8 @@ public class Human {
                 ", surname='" + surname + '\'' +
                 ", year=" + birthDate +
                 ", iq=" + iq +
-                ", schedule=" + Arrays.toString(nonWorkingActivities) +
+                ", schedule=" + Arrays.toString(activities) +
                 '}';
     }
+
 }
