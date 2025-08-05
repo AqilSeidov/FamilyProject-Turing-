d9cb19551704219a4a4a68781fbbc59acb78fde3
Aqil Səidov <aqilasa638@gmail.com>
2025-08-05 18:01:32 +0400

    Enums(Species, DaysOfWeek) added

---
+++ src/main/java/projects/pet/Species.java
@@ -0,0 +1,25 @@
+package projects.pet;
+
+public enum Species {
+
+    DOG,
+    CAT,
+    HAMSTER,
+    RABBIT,
+    GUINEA_PIG,
+    PARROT,
+    CANARY,
+    GOLDFISH,
+    TURTLE,
+    FERRET,
+    CHICKEN,
+    DUCK,
+    GOOSE,
+    PIG,
+    HORSE,
+    SHEEP,
+    GOAT,
+    COW,
+    DONKEY
+}
+
